fdf082d441e88cbb955fd116015d808aa566afe2
Sidduu-Hiremath <104383952+Sidduu-Hiremath@users.noreply.github.com>
2024-09-04 22:57:59 +0530

    Update Soundex.h

--- Soundex.h
+++ Soundex.h
@@ -1,47 +1,14 @@
-#ifndef SOUNDEX_H
-#define SOUNDEX_H
+#ifndef SOUNDEX_HPP
+#define SOUNDEX_HPP
 
 #include <string>
 
-class Soundex {
-public:
-    // Constructor
-    Soundex();
+// Declaration of functions
+char getSoundexCode(char c);
+bool SoundexLengthCheck(const std::string& soundex);
+bool SoundexCodeCheck(char code, char prevCode);
+std::string IncrementSoundex(const std::string& soundex, const std::string& name, char prevCode);
+std::string generateSoundex(const std::string& name);
+std::string padSoundex(const std::string& soundex);
 
-    std::string generateSoundex(const std::string& name);
-
-private:
-
-    static const char soundexMap[26];
-
-    char getSoundexCode(char c) const;
-
-    std::string startSoundex(const std::string& name) const;
-
-    void generateRemainingSoundex(std::string& soundex, const std::string& name) const;
-
-    void processCurrentChar(std::string& soundex, char currentChar, char& prevCode, char& prevPrevChar) const;
-
-    bool shouldAddCode(char prevPrevChar, char currentChar) const;
-
-    void processCharacter(std::string& soundex, char code, char& prevCode) const;
-
-    bool isValidSoundexCode(char code) const;
-
-    bool isNewCode(char code, char prevCode) const;
-
-    bool isSeparatedByHorW(char prevPrevChar) const;
-
-    void padWithZeros(std::string& soundex) const;
-
-    bool isVowel(char c) const;
-
-    bool shouldContinueProcessing(size_t nameLength, size_t soundexLength) const;
-
-    bool shouldAddCode(char currentCode, char prevCode, char prevPrevCode) const;
-
-    bool isShortName(const std::string& name) const;
-    bool canAddCode(char prevPrevCode, char currentCode) const ;
-};
-
-#endif // SOUNDEX_H
+#endif // SOUNDEX_HPP
