00d79d35d5f5f9782ca94b686e1b5b1cde7e7ab7
Sidduu-Hiremath <104383952+Sidduu-Hiremath@users.noreply.github.com>
2024-09-04 22:41:35 +0530

    Update Soundex.Tests.cpp

--- Soundex.Tests.cpp
+++ Soundex.Tests.cpp
@@ -1,12 +1,38 @@
 #include <gtest/gtest.h>
 #include "Soundex.h"
 
-
 TEST(SoundexTest, HandlesEmptyString) {
-    EXPECT_EQ(generateSoundex(""), "");
+    EXPECT_EQ(generateSoundex(""), "0000");
 }
 
 TEST(SoundexTest, HandlesSingleCharacter) {
     EXPECT_EQ(generateSoundex("A"), "A000");
+}
+
+TEST(SoundexTest, HandlesStandardName) {
+    EXPECT_EQ(generateSoundex("Siddu"), "S3");
+}
+
+TEST(SoundexTest, HandlesSimilarSoundingNames) {
+    EXPECT_EQ(generateSoundex("Rupert"), "R163");
+    EXPECT_EQ(generateSoundex("Rubin"), "R150");
+}
+
+TEST(SoundexTest, HandlesDifferentCases) {
+    EXPECT_EQ(generateSoundex("Prajwal"), "P624");
+    EXPECT_EQ(generateSoundex("prajwal"), "P624");
+}
+
+TEST(SoundexTest, HandlesNonCodingCharacters) {
+    EXPECT_EQ(generateSoundex("Tawfgjk"), "T12");
+}
+
+TEST(SoundexTest, HandlesPaddingAndTruncating) {
+    EXPECT_EQ(generateSoundex("Pfister"), "P236");
+    EXPECT_EQ(generateSoundex("Honeyman"), "H555");
+}
+
+TEST(SoundexTest, HandlesNonCodingStart) {
+    EXPECT_EQ(generateSoundex("Euler"), "E460");
 }
 
